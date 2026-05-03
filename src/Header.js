072686f358570717c6490fabfb8e6e56082b0c69
Dieynaba BALDE <dieynababalde36@gmail.com>
2026-05-03 14:09:34 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
     import './Header.css';
 
     function Header() {
-    return (
+      //date du jour
+        const dateJournalier = new Date().toLocaleDateString('fr-FR');
+        return (
         <header className="header">
         <h1 className="header-titre">SenTransport</h1>
         <p className="header-soustitre">
             Votre guide du transport en commun a Dakar
         </p>
+        <p classname="header-date">Date : {dateJournalier}</p>
         </header>
     );
     }
---
+++ src/Statistique.js
@@ -0,0 +1,10 @@
+function Statistique() {
+  return (
+    <div className="stat-item">
+      <h3>10</h3>
+      <p>lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
