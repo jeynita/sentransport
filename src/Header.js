f00c85cbf83d8e1d383efc37fa3513f0f2bf7d13
Dieynaba BALDE <dieynababalde36@gmail.com>
2026-05-28 04:57:16 +0000

    Lab 4 : API Flask avec endpoint GET / lignes

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,18 @@
     import './Header.css';
 
     function Header() {
-      //date du jour
-        const dateJournalier = new Date().toLocaleDateString('fr-FR');
-        return (
-        <header className="header">
+    // Date du jour automatique
+    const dateJournalier = new Date().toLocaleDateString('fr-FR');
+
+    return (
+        <header className="sen-transport-header">
         <h1 className="header-titre">SenTransport</h1>
         <p className="header-soustitre">
             Votre guide du transport en commun a Dakar
         </p>
-        <p classname="header-date">Date : {dateJournalier}</p>
+        <p className="header-date">
+            <span>Date : {dateJournalier}</span>
+        </p>
         </header>
     );
     }
